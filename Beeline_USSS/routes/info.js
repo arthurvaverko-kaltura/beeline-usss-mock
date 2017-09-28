9a2a8a98434d8779ca510ceb13baca84efcfdbed
Sandbox Sync <support@getsandbox.com>
2017-09-28 10:23:20 +0000

    Changes

--- Beeline_USSS/routes/info.js
+++ Beeline_USSS/routes/info.js
@@ -696,7 +696,7 @@ exports.getInfoLogininfo = function(req, res) {
 	res.status(200);
 
 	// set response body and send
-	res.json(req);
+	res.json(req.check('token', 'Invalid parameter').notEmpty());
 };
 
 /*
