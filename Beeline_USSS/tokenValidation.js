e760e6e585a5c041104e64a8b5ae2c0409571bf8
Sandbox Sync <support@getsandbox.com>
2017-09-28 10:50:09 +0000

    Changes

--- Beeline_USSS/tokenValidation.js
+++ Beeline_USSS/tokenValidation.js
@@ -1,12 +1,6 @@
 exports.validateBeelineToken = function(req,res){
-	console.log("req", req);
-	console.log("res", res);
-	console.log("token", req.cookies.token);
-	console.log("cookies", req.cookies);
 	
 	if (!req.cookies.token){
-		console.log("no token found!");
-		res.json({error:"no token"});
-		res.send(401, 'Sorry, no token... no sigar...');
+		req.check('ThereIsNoChanceInHellThisParameterWilllExsistAndThisISTheOnlyWayKnowHowToEndTheResponse', 'Invalid Token').notEmpty();
 	}
 } 
